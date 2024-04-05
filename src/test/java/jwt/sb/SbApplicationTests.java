dd2142ef9a8b32ba857797024c325149b4c46af9
DooHwanKim92 <rmfpsvldj01@naver.com>
2024-04-05 17:23:34 +0900

    test

--- src/test/java/jwt/sb/SbApplicationTests.java
+++ src/test/java/jwt/sb/SbApplicationTests.java
@@ -1,10 +1,35 @@
 package jwt.sb;
 
+import io.jsonwebtoken.security.Keys;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 @SpringBootTest
 class SbApplicationTests {
+
+	@Value("${custom.jwt.secretkey}")
+	private String secretKey;
+
+	@Test
+	@DisplayName("Check Secret Key")
+	void checkSecretKey() {
+		String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKey.getBytes());
+
+		SecretKey testSecretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
+
+		assertThat(testSecretKey).isNotNull();
+		System.out.println(testSecretKey);
+	}
+
 
 	@Test
 	void contextLoads() {
